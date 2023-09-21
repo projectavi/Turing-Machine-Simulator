72610eaaa91d2f68df91800b5221aa5d3b149e7c
Aviraj Newatia <projectavi.exe@gmail.com>
2023-09-21 18:17:05 -0400

    Working on reading config file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 // Defining class names so I can use them wherever
 class TapeCell;
@@ -61,28 +62,34 @@ public:
     TransitionNode* next;
     TransitionNode* prev;
 
-    char* symbol_read;
-    char* symbol_write;
-    char left_right;
+    std::string symbol_read;
+
+    std::string symbol_write;
+    std::string left_right;
     State* next_state;
+    std::string next_state_name;
 };
 
 class State {
 private:
+    std::string name;
+
+public:
     bool is_initial;
     bool is_accept;
     bool is_reject;
-    char* name;
+
+    State* next_state_list;
+    State* prev_state_list;
 
     TransitionNode* transition_list_head;
 
-public:
-    State(bool initial, bool accept, bool reject, char* state_name) {
-        is_accept = accept;
-        is_reject = reject;
-        is_initial = initial;
-        name = state_name;
-        transition_list_head = nullptr;
+    void set_name(std::string new_name) {
+        name = new_name;
+    }
+
+    std::string get_name() {
+        return name;
     }
 };
 
@@ -92,6 +99,7 @@ private:
     State* initial_state;
     State* accept_state;
     State* reject_state;
+    State* state_list_head;
 
 public:
     TuringMachine() {
@@ -101,17 +109,99 @@ public:
         reject_state = nullptr;
     }
 
-    int load_from_file(char* filename) {
+    int load_machine_from_file(char* filename) {
         // Remember to write the input string to the tape first
+        std::ifstream config_file;
+        config_file.open(filename);
+
+        char* delim = (char*) ", ";
+        State *curr_node;
+
+        std::string line;
+        std::string transition_information[5];
+
+        unsigned long pos;
+
+        // Read from config file, this will contain information about state transitions and the transition function
+        // Read in the initial, accept, and reject states
+        for (int i = 0; i < 3; i++) {
+            getline(config_file, line);
+            state_list_head = (State*)(malloc(sizeof(State)));
+            state_list_head->is_initial = true;
+            state_list_head->is_accept = false;
+            state_list_head->is_reject = false;
+            state_list_head->set_name(line);
+
+            getline(config_file, line);
+            state_list_head->next_state_list = (State*)(malloc(sizeof(State)));
+            curr_node = state_list_head->next_state_list;
+            curr_node->is_initial = false;
+            curr_node->is_accept = true;
+            curr_node->is_reject = false;
+            curr_node->set_name(line);
+
+            getline(config_file, line);
+            curr_node->next_state_list = (State*)(malloc(sizeof(State)));
+            curr_node = curr_node->next_state_list;
+            curr_node->is_initial = false;
+            curr_node->is_accept = true;
+            curr_node->is_reject = false;
+            curr_node->set_name(line);
+        }
+
+        while (getline(config_file, line)) {
+            for (auto & i : transition_information) {
+                while (pos < line.size()) {
+                    pos = line.find(delim);
+                    i = line.substr(0, pos);
+                    line.erase(0, pos + 2);
+                }
+            }
+
+            // Build a transition object
+
+            // Find the state in the state linked list
+            bool new_state_needed = true;
+            curr_node = state_list_head;
+            TransitionNode* curr_transition_node;
+            while (curr_node->next_state_list != nullptr) {
+                if (curr_node->get_name() == transition_information[0]) {
+                    new_state_needed = false;
+                    curr_transition_node = curr_node->transition_list_head;
+                    while (curr_transition_node->next != nullptr) {
+                        curr_transition_node = curr_transition_node->next;
+                    }
+                    curr_transition_node->next = (TransitionNode*) (malloc(sizeof (TransitionNode)));
+                    curr_transition_node = curr_transition_node->next;
+                    curr_transition_node->symbol_read = transition_information[1];
+                    curr_transition_node->next_state_name = transition_information[2];
+                    curr_transition_node->symbol_write = transition_information[3];
+                    curr_transition_node->left_right = transition_information[4];
+                    break;
+                }
+                curr_node = curr_node->next_state_list;
+            }
+        }
         return -1;
     }
 
-
-    // TODO - figure out how to do transitions, either treat each transition as a class and create based on read or bake it into the states, i think its easier to make a class for transitions
+    int load_configuration(char* filename) {
+        // Read config file, this will allow the machine to be loaded in different configuraitons
+        return -1;
+    }
 
 };
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cout << "Incorrect number of arguments, please enter a filename" << std::endl;
+    }
+
+    char* filename = argv[1];
+
+    auto* TM = new TuringMachine();
+
+    TM->load_machine_from_file(filename);
+
     return 0;
 }
